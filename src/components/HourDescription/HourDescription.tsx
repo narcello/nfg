7470a1aaf68d5de2c5cf024b5b199dac6d55682f
narcello <marcellovcs@gmail.com>
2020-11-15 21:40:21 -0300

    chore: refactor typo error

--- src/components/HourDescription/HourDescription.tsx
+++ src/components/HourDescription/HourDescription.tsx
@@ -5,7 +5,7 @@ let hourBlock = [
   {mainHour: '08:00', halfHour: '08:30', period: 'am'},
   {mainHour: '09:00', halfHour: '09:30', period: 'am'},
   {mainHour: '10:00', halfHour: '10:30', period: 'am'},
-  {mainHour: '11:00', halfHour: '10:30', period: 'am'},
+  {mainHour: '11:00', halfHour: '11:30', period: 'am'},
   {mainHour: '12:00', halfHour: '12:30', period: 'pm'},
   {mainHour: '01:00', halfHour: '01:30', period: 'pm'},
   {mainHour: '02:00', halfHour: '02:30', period: 'pm'},
